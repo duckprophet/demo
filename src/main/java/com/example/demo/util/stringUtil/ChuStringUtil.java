9aed6d53b7f8fa143e92264632f6566a76a0033d
duckprophet <806587644@qq.com>
2021-02-19 12:34:14 +0800

    hello,git

---
+++ src/main/java/com/example/demo/util/stringUtil/ChuStringUtil.java
@@ -0,0 +1,30 @@
+package com.example.demo.util.stringUtil;
+
+/**
+ * ClassName:ChuStringUtil
+ * Description:字符串相关工具方法
+ * @date:2021/2/19 11:19
+ * @author: chuliyi
+ */
+public class ChuStringUtil {
+    
+    /**
+     * @author : chuliyi
+     * @version : v1.0
+     * @description 如果一个字符串去除空格后以数字或者"."开头，当以数字开头的时候返回第一个浮点序列，当以"."开头默认小数点前为0并返回第一个浮点序列，如果不是以数字或者小数点开头则返回"0"
+     * @date : 11:20 2021/2/19
+     */
+    public static String getFirstFloFromStr(String str){
+        String tmp = str.trim();
+        String ret = "";
+        for (int i = 0; i < tmp.length(); i++) {
+            if (tmp.charAt(i) == 46){
+
+            }
+        }
+        return  ret;
+    }
+
+
+    
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,8 +3,17 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class DemoApplicationTests {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        BigDecimal a = new BigDecimal("");
+        System.out.println(a);
+
+    }
+
 
     @Test
     void contextLoads() {
